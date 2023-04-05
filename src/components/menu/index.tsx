4e268724ab76f619df60556d1d950ac6d1274645
Alex Lancao <lancao@shoppersupply.com.br>
2023-04-05 17:48:12 -0300

    feat: static menu

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,53 +1,81 @@
 import Image from "next/image";
-import { HiOutlineUserGroup } from "react-icons/hi";
+import {
+  HiOutlineUserGroup,
+  HiOutlineKey,
+  HiOutlineSparkles,
+  HiOutlineExclamationCircle,
+  HiOutlineSaveAs,
+  HiOutlineShoppingBag,
+  HiOutlineQuestionMarkCircle,
+  HiOutlineUser,
+} from "react-icons/hi";
+import { IoMdAddCircle } from "react-icons/io";
+import Logo_Shopper from "../../../public/Logo_Shopper.svg";
 
 const Menu = () => {
   return (
-    <div className="fixed w-[21.2rem] h-screen drop-shadow-xl">
-      <h1>Shopper Supplay</h1>
-      <ul>
-        <li>
-          <HiOutlineUserGroup color="#5F4B8B" />
-          Curadores
-        </li>
-        <li>
-          <Image src="" alt="" />
-          Qualidade
-        </li>
-        <li>
-          <Image src="" alt="" />
-          Permissoes
-        </li>
-        <li>
-          <Image src="" alt="" />
-          Erros
-        </li>
-      </ul>
+    <div className="flex flex-col justify-between fixed w-[21.2rem] h-screen drop-shadow-xl bg-branco-primario text-roxo-primario text-[1.8rem]">
       <div>
+        <Image
+          src={Logo_Shopper}
+          alt="Logo Shopper Supply"
+          className="mt-[3.9rem]"
+        />
+        <ul className="flex flex-col gap-[2rem] mt-[3.5rem]">
+          <li className="flex items-center gap-[1rem] mx-[2.2rem] cursor-pointer">
+            <HiOutlineUserGroup color="#5F4B8B" size="2.5rem" />
+            Curadores
+          </li>
+          <li className="flex items-center gap-[1rem] mx-[2.2rem] cursor-pointer">
+            <HiOutlineSparkles color="#5F4B8B" size="2.5rem" />
+            Qualidade
+          </li>
+          <li className="flex items-center gap-[1rem] mx-[2.2rem] cursor-pointer">
+            <HiOutlineKey color="#5F4B8B" size="2.5rem" />
+            Permissoes
+          </li>
+          <li className="flex items-center gap-[1rem] mx-[2.2rem] cursor-pointer">
+            <HiOutlineExclamationCircle color="#5F4B8B" size="2.5rem" />
+            Erros
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-col gap-[2rem]">
         <label
           htmlFor="dropzone-file"
-          className="mx-auto cursor-pointer flex w-full max-w-lg flex-col items-center rounded-xl border-2 border-dashed border-roxo-primario p-6 text-center"
+          className="mx-[2.2rem] px-[1rem] py-[.6rem] cursor-pointer flex justify-around max-w-lg flex-col items-center rounded-xl border-2 border-dashed border-roxo-primario text-center"
         >
-          <h2 className="mt-4 text-xl text-roxo-primario font-medium tracking-wide">
+          <h2 className="mt-4 text-[1.5rem] text-roxo-primario font-medium tracking-wide">
             Revisar planilha
           </h2>
-          <p className="mt-2 tracking-wide">
+          <p className="mt-2 text-[.9rem] text-roxo-primario opacity-80 tracking-wide">
             Arraste e soute ou clique, para enviar uma planilha para revisão.
           </p>
+          <IoMdAddCircle color="#5F4B8B" size="3.5rem" />
           <input id="dropzone-file" type="file" className="hidden" />
         </label>
-        <ul>
-          <li>
-            <Image src="" alt="" />
+        <ul className="flex flex-col gap-[2rem] ">
+          <li className="flex items-center gap-[1rem] ml-[2.2rem] opacity-50 cursor-pointer">
+            <HiOutlineSaveAs color="#5F4B8B" size="2.5rem" />
+            Comparação
+          </li>
+          <li className="flex items-center gap-[1rem] ml-[2.2rem] cursor-pointer">
+            <HiOutlineShoppingBag color="#5F4B8B" size="2.5rem" />
             Padrão de Sacola
           </li>
-          <li>
-            <Image src="" alt="" />
+          <li className="flex items-center gap-[1rem] mx-[2.2rem] cursor-pointer">
+            <HiOutlineQuestionMarkCircle color="#5F4B8B" size="2.5rem" />
             Help
           </li>
-          <div>
-            <input type="checkbox" />
-            <button>User</button>
+          <div className="flex justify-around items-center mb-[2rem] ">
+            <input
+              type="checkbox"
+              className="cursor-pointer appearance-none w-[7rem] h-[4rem] focus:outline-none checked:bg-branco-primario bg-roxo-primario  rounded-full before:inline-block before:rounded-full before:bg-branco-primario before:h-[3.6rem] before:w-[3.6rem] checked:before:bg-roxo-primario checked:before:translate-x-[3rem] drop-shadow-md transition-all duration-500 before:ml-[.2rem] before:mt-[.2rem]"
+            />
+
+            <button className="bg-branco-secundario p-[1.1rem] rounded-full drop-shadow-sm hover:bg-roxo-primario hover:text-branco-primario transition-colors">
+              <HiOutlineUser size="2.5rem" />
+            </button>
           </div>
         </ul>
       </div>
