b7ebd4f57d53134f1bda9001fbdd808350eca655
Alex Lancao <lancao@shoppersupply.com.br>
2023-04-16 04:02:43 -0300

    feat: funcional select

--- src/components/modalEnvioErros/index.tsx
+++ src/components/modalEnvioErros/index.tsx
@@ -8,64 +8,18 @@ import { curadores, errosTypes, places } from "./db";
 const ModalEnvioErros = () => {
   // esse modal precisa receber um provider com um array com todos os curadores ativos, erros de QA, Cliente, Shopping, ABBR e Loja.
 
-  interface IoptionsEerrosTypes {
-    id: number;
-    group: string;
-    title: string;
-    description: string;
-    severity: number;
-    collector: string;
-    label: string;
-  }
-  interface IoptionsCuradores {
-    id: number;
-    name: string;
-    label: string;
-  }
+  const [curatorValue, setCuratorValue] = useState("");
 
-  interface IoptionsPlaces {
-    id: string;
-    client: string;
-    mall: string;
-    abbr: string;
-    name: string;
-    is_active: boolean;
-    label: string;
-  }
+  const onChange = (event : any) => {
+    setCuratorValue(event.target.value);
+  };
 
-  const [selectedOptionState, setSelectedOptionState] = useState<
-    IoptionsEerrosTypes[] | IoptionsCuradores[] | IoptionsPlaces[] | null
-  >([]);
+  const onClick = (event: any) => {
+    setCuratorValue(event)
+  }
 
   // Funções responsaveis com formatar o objeto e adicionar o campo label.
-  const optionsCuradores: IoptionsCuradores[] = curadores.map((curador) => ({
-    ...curador,
-    label: curador.name,
-  }));
-
-  const optionsPlacesCliente: IoptionsPlaces[] = places.map((place) => ({
-    ...place,
-    label: place.client,
-  }));
-   const optionsPlacesProject: IoptionsPlaces[] = places.map((place) => ({
-     ...place,
-     label: place.abbr,
-   }))
-
-  const optionsErrosTypes: IoptionsEerrosTypes[] = errosTypes.map((error) => ({
-    ...error,
-    label: `${error.group} | ${error.title}`,
-  }));
-
-  const handleChange = (
-    selectedOption: SingleValue<
-      IoptionsEerrosTypes | IoptionsCuradores | IoptionsPlaces
-    >
-  ) => {
-    setSelectedOptionState(banana => [...banana, selectedOption]);
-    return true;
-  };
-  console.log(selectedOptionState);
+ 
   return (
     <div className="flex flex-col justify-center items-center gap-[3rem] w-[25%] h-screen bg-branco-primario drop-shadow-md">
       <Image src={iconRobo} alt="Incone robô de qualidade Shopper" />
@@ -74,47 +28,67 @@ const ModalEnvioErros = () => {
         informações estão corretas.
       </p>
       <form className="flex flex-col justify-center items-center w-[85%]">
-        <Select
-          options={optionsCuradores}
-          onChange={handleChange}
-          className="w-[100%] rounded-full border-roxo-primario border-[.2rem] h-[4rem] text-[1.8rem] text-roxo-primario before:border-none after:border-none hover:none"
-          styles={{
-            control: (baseStyles, state) => ({
-              ...baseStyles,
-              background: "transparent",
-              border: 0,
-              boxShadow: "none",
-            }),
-          }}
-        />
-        <Select
-          options={optionsErrosTypes}
-          onChange={handleChange}
-          className="w-[100%] rounded-full border-roxo-primario border-[.2rem] h-[4rem] text-[1.8rem] text-roxo-primario before:border-none after:border-none hover:none"
-          styles={{
-            control: (baseStyles, state) => ({
-              ...baseStyles,
-              background: "none",
-              border: 0,
-              boxShadow: "none",
-            }),
-          }}
-        />
-        <fieldset>
-          <Select
-            options={optionsPlacesCliente}
-            onChange={handleChange}
-            className="w-[100%] rounded-full border-roxo-primario border-[.2rem] h-[4rem] text-[1.8rem] text-roxo-primario before:border-none after:border-none hover:none"
-            styles={{
-              control: (baseStyles, state) => ({
-                ...baseStyles,
-                background: "none",
-                border: 0,
-                boxShadow: "none",
-              }),
-            }}
+        <div>
+          <input
+            type="text"
+            value={curatorValue}
+            onChange={onChange}
+            className="w-[100%] rounded-full border-roxo-primario px-[1rem] border-[.2rem] h-[4rem] text-[1.8rem] text-roxo-primario focus:border-roxo-primario focus:outline-none"
           />
-        </fieldset>
+          <div
+            className={`${
+              curatorValue ? "border-[.2rem] border-roxo-primario" : " "
+            } transition-transform rounded-lg mt-[.5rem] drop-shadow-lg z-10 absolute w-[80%]`}
+          >
+            {curadores
+              .filter((curador) => {
+                const searchTerm = curatorValue.toLocaleLowerCase();
+                const fullname = curador.name.toLowerCase();
+
+                return searchTerm && fullname.startsWith(searchTerm);
+              })
+              .map((curador) => (
+                <div
+                  key={curador.id}
+                  onClick={() => onClick(curador.name)}
+                  className="relative text-[1.8rem]  px-[1rem] text-roxo-primario hover:bg-roxo-primario hover:text-branco-primario cursor-pointer"
+                >
+                  {curador.name}
+                </div>
+              ))}
+          </div>
+        </div>
+
+        <div>
+          <input
+            type="text"
+            value={curatorValue}
+            onChange={onChange}
+            className="w-[100%] rounded-full border-roxo-primario px-[1rem] border-[.2rem] h-[4rem] text-[1.8rem] text-roxo-primario focus:border-roxo-primario focus:outline-none"
+          />
+          <div
+            className={`${
+              curatorValue ? "border-[.2rem] border-roxo-primario" : " "
+            } transition-transform rounded-lg mt-[.5rem] drop-shadow-lg z-10 absolute w-[80%]`}
+          >
+            {curadores
+              .filter((curador) => {
+                const searchTerm = curatorValue.toLocaleLowerCase();
+                const fullname = curador.name.toLowerCase();
+
+                return searchTerm && fullname.startsWith(searchTerm);
+              })
+              .map((curador) => (
+                <div
+                  key={curador.id}
+                  onClick={() => onClick(curador.name)}
+                  className="relative text-[1.8rem]  px-[1rem] bg-white text-roxo-primario hover:bg-roxo-primario hover:text-branco-primario cursor-pointer"
+                >
+                  {curador.name}
+                </div>
+              ))}
+          </div>
+        </div>
       </form>
     </div>
   );
