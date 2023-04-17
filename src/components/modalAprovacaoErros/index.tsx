759bcdfebf537c5e62befeee18a664a160cf7c12
Alex Lancao <lancao@shoppersupply.com.br>
2023-04-17 17:21:47 -0300

    feat: static modal aprove errors

--- src/components/modalAprovacaoErros/index.tsx
+++ src/components/modalAprovacaoErros/index.tsx
@@ -8,27 +8,28 @@ import { useModal } from "@/providers/modaisProvider";
 const ModalAprovacaoErros = () => {
   return (
     <div
-      className={`flex flex-row-reverse ${errors.length > 0 ? "" : "hidden"}`}
+      className={`flex flex-row-reverse ${!errors && "hidden"}`}
     >
-      <div className="p-4 flex flex-col justify-center items-center gap-[3rem] w-[25%] h-screen bg-branco-primario drop-shadow-md">
-        <div className="overflow-y-auto flex flex-col h-[90vh] w-[100%]">
+      <div className=" flex flex-col justify-center items-center w-[25%] h-screen bg-branco-primario drop-shadow-md">
+        <div className="overflow-y-auto flex flex-col w-[100%] text-roxo-primario text-[1.2rem] font-semibold">
           {errors?.map((erro, i) => {
             return (
-              <div className="flex" key={i}>
+              <div className="flex justify-between items-center bg-branco-secundario p-5 my-2 mx-4 rounded-md" key={i}>
                 <div
-                  className={`rounded-full bg-severity-2 w-[2.3rem] h-[2.3rem]`}
+                  className={`rounded-full shadow-inner bg-severity-${erro.error_type.severity} w-[2.3rem] h-[2.3rem]`}
+                  title={`Erro de relevancia ${erro.error_type.severity}`}
                 ></div>
-                <div>
+                <div className="text-center">
                   <p>{erro.sheet}</p>
                   <p>{erro.coor}</p>
                 </div>
-                <p>{erro.error_type.title}</p>
-                <HiOutlineTrash />
+                <p className="w-[60%] text-center">{erro.error_type.title}</p>
+                <HiOutlineTrash size="2rem" className="cursor-pointer"/>
               </div>
             );
           })}
         </div>
-        <div className="flex">
+        <div className="flex items-center content-center justify-center w-[100%] py-6 gap-3">
           <div className="drop-shadow-md rounded-full font-bold bg-branco-primario text-roxo-primario p-4 cursor-pointer">
             <HiArrowUturnLeft size="2rem" fontWeight="900" />
           </div>
@@ -43,7 +44,6 @@ const ModalAprovacaoErros = () => {
           </div>
         </div>
       </div>
-      <div className="h-screen w-3/4 backdrop-blur-sm bg-black bg-opacity-25"></div>
     </div>
   );
 };
